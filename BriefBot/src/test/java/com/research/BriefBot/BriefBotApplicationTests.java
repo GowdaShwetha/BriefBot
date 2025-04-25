fcf80a2118c067d988b7342e6f3106ba27d02c1c
GowdaShwetha <shwethagowda252001@gmail.com>
2025-04-25 22:51:17 +0530

    commit

---
+++ BriefBot/src/test/java/com/research/BriefBot/BriefBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.research.BriefBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BriefBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
